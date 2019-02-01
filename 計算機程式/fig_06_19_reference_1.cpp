286400a102f47f4fafe63a452d9c666e71ed6d32
ChingYuTsai <cytsai0703@gmail.com>
2019-02-01 17:17:35 +0800

    add fig_06_19

---
+++ 計算機程式/fig_06_19_reference_1.cpp
@@ -0,0 +1,32 @@
+//fig06_19.cpp
+//compare pass by value and pass by reference 
+#include <iostream>
+using namespace std;
+
+int squareByValue( int );
+void squareByReference( int &);
+
+int main()
+{
+    int x = 2;
+    int z = 4;
+
+    cout << "x= " << x << " before squareByValue\n";
+    cout << "Value returned by squareByValue: " << squareByValue( x ) << endl;
+    cout << "x = " << x << " after squareByValue\n" << endl;
+
+    cout << "z= " << z << " before squareByReference\n";
+    squareByReference( z );
+    cout << "z = " << z << " after squareByReference\n" << endl;
+    return 0;
+}
+
+int squareByValue( int number )
+{
+    return number *= number;
+}
+
+void squareByReference( int &numberRef)
+{
+    numberRef *= numberRef;
+}
